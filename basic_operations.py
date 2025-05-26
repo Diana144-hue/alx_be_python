051d1dddbd7aef91d2ccc89117293cef8a2388c4
Diana Chepngeno <dianachepngeno251@gmail.com>
2025-05-26 19:49:03 +0300

    python introduction

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 
+number2 = 5
+Addition = number1 + number2
+Substraction = number2 - number1
+Multiplication = number1 * number2
+
+print("sum of number1 and number2 is", Addition)
+print("Substraction of number2 and number1 is", Substraction)
+print("Multiplication of number1 and number2 is", Multiplication)
