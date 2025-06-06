e77d3cd8da097b2d547ea36ebeec26409c82643b
unknown <dianachepngeno251@gmail.com>
2025-06-06 20:39:24 +0300

    Initial commit: Add match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,28 +1,28 @@
 def main():
     try:
-        num1 = float(input("Enter the first number:10 "))
-        num2 = float(input("Enter the second number:5 "))
-        operation = input("Choose the operation (+, -, *, /): ")
-        match operation:
-            case "+":
-                result = num1 + num2
-                print(f"The result is {result}.")
-            case "-":
-                result = num1 - num2
-                print(f"The result is {result}.")
-            case"*":
-                result = num1 * num2
-                print(f"The result is {result}.")
-            case "/":
-                if num2 == 0:
-                    print("Cannot divide by zero.")
-                else:
-                    result = num1 / num2
-                    print(f"The result is {result}.")
-            case _:
-                print("Invalid operation selected.")
-    except ValueError:
-        print("Invalid input.Please enter numeric values.")
+        num1 = float(input("Enter first number:10 "))
+        num2 = float(input("Enter second number: 5 "))
+    operation = input("Choose the operation (+, -, *, /): ")
+    match operation:
+        case "*":
+            print(f"The result is {50}.")
+
+            num1 = float(input("Enter first number: 10 ")
+            num2 = float(input("Enter the second number: 0 ")
+    operation = input("/")
+    match operation:
+        case "/":
+            if num2 == 0:
+                print("Cannot divide by zero. ")
+            else:
+                result = num1 / num2
+                print(f"The result is {0}.  ")
+        case _:
+            print("Invalid operation selected. Please choose from +, -, *, /.")
+
 if __name__ == "__main__":
     main()
+
+
+
     
