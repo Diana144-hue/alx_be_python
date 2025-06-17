4526117e6aad73015b4ce80e812d2ae564a1114e
unknown <dianachepngeno251@gmail.com>
2025-06-17 11:41:51 +0300

    Initial commit: Add perform_operation function

--- arithmetic_operation.py
+++ arithmetic_operation.py
@@ -1,25 +1,16 @@
+# arithmetic_operation.py
+
 def perform_operation(num1, num2, operation):
-    """
-    
-    perfom basic arithmetic operations between two numbers.
-    
-    parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
-        operation(str): The operation to perform 'add', 'subtract',
-        
-    Returns:
-        float or str: The result of the operation, or a specific message in case of error.
-    """
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
-        return num * num2
+        return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2
     else:
         return "Error: Invalid operation"
+    
