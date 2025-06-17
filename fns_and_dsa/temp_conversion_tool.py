fc12a2f63a8c9f55957b8d55668b3784d2916a17
Diana144-hue <dianachepngeno251@gmail.com>
2025-06-17 15:15:08 +0300

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,34 +1,35 @@
-# Global convesion factor                                               
+# Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit -32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-  return(celsius * CELSIUS_TO_FARHENEIT_FACTOR) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
     try:
         temp_input = input("Enter the temperature to convert: ").strip()
         temperature = float(temp_input)
-        unit = input("Is the temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
         if unit == 'F':
-           celsius = convert_to_celsius(temperature)
-           print(f"'{temperature}°F is {celsius:.2f}°c")
+            celsius = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {celsius:.2f}°C")
         elif unit == 'C':
-             fahrenheit = convert_to_farenheit(temperature)
-             print(f"{temperature}°C is {fahrenheit:.2f}F")
+            fahrenheit = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {fahrenheit:.2f}°F")
         else:
-            raise ValueError("Invalid temperature unit.Please enter 'C' or 'F'.")
+            raise ValueError("Invalid temperature unit. Please enter 'C' or 'F'.")
 
-     except ValueError as e:
-         print("Invalid temperature.Please enter a numeric value.")
+    except ValueError as e:
+        print("Invalid temperature. Please enter a numeric value.")
 
 
-  if __name__ == "__main__":
-       main()
+if __name__ == "__main__":
+    main()
+
     
       
 
