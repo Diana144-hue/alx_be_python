e04b8963906480ae9c5a6c067db051099d7c7a37
Diana144-hue <dianachepngeno251@gmail.com>
2025-06-24 14:58:08 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,4 +1,84 @@
+<<<<<<< HEAD
+import sys
 
+=======
+>>>>>>> 9ffc70e (Save changes before rebase)
+class BankAccount:
+    def __init__(Self, initial_balance=0):
+         self.__account_balance = initial_balance
+
+    def depossit(self, amount):
+        if amount > 0:
+          self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance -= amount
+              self.__account_balance -= amount
+              return True
+        return False
+
+     def display_balance(self):
+<<<<<<< HEAD
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+
+def main():
+    account = BankAccount(100) # Starting balance
+
+    if len(sys.argv) < 2:
+       print("Usage: python combine_bank_app.py <command>:<amount>")
+=======
+       print(f"Current Balance: ${self.__account_balance}")
+import sys
+from bank_account import BankAccount
+
+def main():
+    account = BankAccount(100) # Example starting balance
+
+    if len(sys.argv) < 2:
+        print("Usage: python main-0.py <command>:<amount>")
+>>>>>>> 9ffc70e (Save changes before rebase)
+        print("Commands: deposit, withdraw, display")
+        sys.exit(1)
+
+    command, *params = sys.argv[1].split(':')
+    amount = float(params[0] if params else None
+
+     if command == "deposit" and amount is not None:
+        account.deposit(amount)
+<<<<<<< HEAD
+        print(f"Deposited: ${amount:.2f}")
+    elif command == "Withdraw" and amount is not None:
+       if account.withdraw(amount):
+           print(f"Withdrew: ${amount:.2f}")
+=======
+         print(f"Deposited: ${amount}")
+elif command == "withdraw" and amount is not None:
+        if account.withdraw(amount):
+             print(f"Withdraw: ${amount}")
+>>>>>>> 9ffc70e (Save changes before rebase)
+        else:
+              print("Insufficient funds.")
+      elif command == "display":
+            account.display_balance()
+      else:
+           print("Invalid command.")
+
+if__name__ == "__main__":
+    main()
+  
+
+
+
+
+
+        
+      
+
+
+    
+       
+       
+         
 
                   
   
