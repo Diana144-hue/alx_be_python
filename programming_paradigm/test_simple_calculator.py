20fafed93bcd4e3900e4038706af3f4fc7f00336
Diana144-hue <dianachepngeno251@gmail.com>
2025-06-26 12:40:09 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,4 +8,39 @@ class TestSimpleCalculator(unittest.Testcase):
       self.calc = SimpleCalculator()
 
   def test_addition(self):
-    
+      """Test the addition method."""
+      self.assertEqual(self.calc.add(2, 3), 2)
+      self.assertEqual(self.calc.add(-1, 1), 0)
+      self.assertEqual(self.calc.add(-5, -5), -10)
+      self.assertEqual(self.calc.add(0, 0), 0)
+
+  def test_subtraction(self):
+      """Test the sutraction method."""
+      self.assertEqual(self.calc.subtract(10, 5), 5)
+      self.assertEqual(self.calc.subtract(-1, -1), 0)
+      self.assertEqual(self.calc.subtract(0, 5), -5)
+      self.assertEqual(self.calc.subtract(3, 10), -7)
+
+  def test_multiplication(self):
+      """Test the multiplication method."""
+      self.assertEqual(self.calc.multiply(4, 5), 20)
+      self.assertEqual(self.calc.multiply(-2, 3), -6)
+      self.assertEqual(self.calc.multiply(0, 100), 0)
+      self.asserttEqual(self.calc.multiply(-3 ,-3), 9)
+
+  def test_division(self):
+       """Test the division method."""
+       self.assertEqual(self.calc.divide(10, 2), 5)
+       self.assertEqual(self.calc.divide(-9, 3), -3)
+       self.assertEqual(self.calc.divide(0, 5), 0)
+       self.assertEqual(self.calc.divide(7, 2), 3.5)
+  
+  def test_division_by_zero(self)
+      """Test division by zero returns None."""
+      self.asserIsNone(self.calc.divide(5, 0))
+      self.assertIsNone(self.calc.divide(0, 0))
+
+if__name__ == '__main__':
+    unittest.main()
+
+      
